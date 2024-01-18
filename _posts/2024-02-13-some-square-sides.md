a119a0e61a031192cd754d46e52f63903cfdb94b
Jack Hong <jackhong@alum.ubc.ca>
2024-01-18 15:31:55 -0800

    Quest 3 puzzles done

--- _posts/2024-02-13-some-square-sides.md
+++ _posts/2024-02-13-some-square-sides.md
@@ -9,20 +9,54 @@ gem_img: gem3.png
 desmos_link: https://www.desmos.com/calculator/ejxejkil35?invertedColors=true
 ---
 
-<div id="puzzle1">
+
+
+<div id="puzzle1" class="puzzle">
     <div class="block--isDraggable">0</div>
     <div class="block--isDraggable">2</div>
     <div class="block--isDraggable">4</div>
     <div class="block--isDraggable">5</div>
-    <div class="block notDraggable"><button id="submit1">Check My Answer</button></div>
+    <div class="block notDraggable top bot left right"><button id="submit1">Check My Answer</button></div>
     <div class="block--isDraggable">7</div>
     <div class="block--isDraggable">9</div>
     <div class="block--isDraggable">11</div>
     <div class="block--isDraggable">14</div>
 </div>
 
-<div id="usr-msg1" class="usr-msg">Touching pairs must sum to a square number.</div>
+<div id="usr-msg1" class="usr-msg">
+    Adjacent pairs must sum to a square number.
+</div>
 
 <div id="part2">
-asfsdf  
+<div id="puzzle2" class="puzzle">
+    <div class="block--isDraggable">0</div>
+    <div class="block--isDraggable">1</div>
+    <div class="block--isDraggable">2</div>
+    <div class="block--isDraggable">3</div>
+    <div class="block--isDraggable">4</div>
+    <div class="block--isDraggable">5</div>
+        <div class="block notDraggable top left"></div>
+        <div class="block notDraggable top"></div>
+        <div class="block notDraggable top right"></div>
+    <div class="block--isDraggable">6</div>
+    <div class="block--isDraggable">7</div>
+        <div class="block notDraggable left"></div>
+        <div class="block notDraggable"><button id="submit2">Check My Answer</button></div>
+        <div class="block notDraggable right"></div>
+    <div class="block--isDraggable">9</div>
+    <div class="block--isDraggable">10</div>
+        <div class="block notDraggable left bot"></div>
+        <div class="block notDraggable bot"></div>
+        <div class="block notDraggable right bot"></div>
+    <div class="block--isDraggable">11</div>
+    <div class="block--isDraggable">12</div>
+    <div class="block--isDraggable">13</div>
+    <div class="block--isDraggable">14</div>
+    <div class="block--isDraggable">15</div>
+    <div class="block--isDraggable">16</div>
+</div>
+<div id="usr-msg2" class="usr-msg">
+    Adjacent pairs must sum to a square number.
+</div>
+
 </div>
--- assets/js/quest3.js
+++ assets/js/quest3.js
@@ -9,11 +9,24 @@ const swappable1 = new Swappable(document.getElementById('puzzle1'), {
   },
 });
 
-const puzzle1 = document.getElementById("puzzle1");
+const swappable2 = new Swappable(document.getElementById('puzzle2'), {
+  draggable: '.block--isDraggable',
+  mirror: {
+    constrainDimensions: true,
+  },
+});
+
 const submit1 = document.getElementById("submit1");
+const submit2 = document.getElementById("submit2");
 const usr_msg1 = document.getElementById("usr-msg1");
+const usr_msg2 = document.getElementById("usr-msg2");
 
 const adj1 = [[1,3],[0,2],[1,5],[0,6],[],[2,8],[3,7],[6,8],[5,7]];
+const adj2 = [[1,5],   [0,2],   [1,3],   [2,4],   [3,9],
+              [0,10],  [],      [],      [],      [4,14],
+              [5,15],  [],      [],      [],      [9,19],
+              [10,20], [],      [],      [],      [14,24],
+              [15,21], [20,22], [21,23], [22,24], [19,23]]
 const squares = new Set(Array.from({length: 10}, (x, i) => i*i));
 
 const part2 = document.getElementById("part2");
@@ -30,16 +43,19 @@ if (solve_status == 'solved_part_1') {
 }
 
 submit1.addEventListener("click", checkPuzzle1);
+submit2.addEventListener("click", checkPuzzle2);
+
 
 function checkPuzzle1() {
   const blocks = document.querySelectorAll('#puzzle1 > div'); 
   for (let i=0; i<blocks.length; i++) {
-    if (i === 4) { continue; }
+    if (blocks[i].classList.contains("notDraggable")) { continue; }
+
     const n = Number(blocks[i].innerHTML);
     const n1 = Number(blocks[adj1[i][0]].innerHTML);
     const n2 = Number(blocks[adj1[i][1]].innerHTML);
     if (!squares.has(n+n1) || !squares.has(n+n2)) {
-      usr_msg1.innerHTML = "Touching pairs must sum to a square number.";
+      usr_msg1.innerHTML = "Adjacent pairs must sum to a square number.";
       flashError(usr_msg1);
       return;
     }
@@ -55,3 +71,27 @@ function checkPuzzle1() {
     }
 }
 
+function checkPuzzle2() {
+  const blocks = document.querySelectorAll('#puzzle2 > div'); 
+  for (let i=0; i<blocks.length; i++) {
+    if (blocks[i].classList.contains("notDraggable")) { continue; }
+
+    const n = Number(blocks[i].innerHTML);
+    const n1 = Number(blocks[adj2[i][0]].innerHTML);
+    const n2 = Number(blocks[adj2[i][1]].innerHTML);
+    if (!squares.has(n+n1) || !squares.has(n+n2)) {
+      usr_msg2.innerHTML = "Adjacent pairs must sum to a square number.";
+      flashError(usr_msg2);
+      return;
+    }
+  }
+  usr_msg2.innerHTML = "You did it!"
+  showCorrect(usr_msg2);
+  gem_wrapper.style.display = "grid";
+    if (readCookie("quest3") == null) {
+        gem_wrapper.scrollIntoView();
+        if (readCookie('cookie-notice-option') == "true") {
+            createCookie("quest3", "solved_part_2", 365);
+        }
+    }
+}
